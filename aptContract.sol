97db3ff095640a93ef906d161780467e84798be6
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-11-24 00:34:36 -0500

    ran prettier to auto-format file

--- aptContract.sol
+++ aptContract.sol
@@ -1,24 +1,25 @@
-pragma solidity >=0.4.22 < 0.7.0;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.8.0;
 
 contract aptContract {
     struct Tenant {
         string name;
         address payable tenant;
-        uint lease_length; //in months 
+        uint256 lease_length; //in months
     }
-    
+
     address payable public owner;
-    
-    Tenant[] tenants;  //list of tenants to auto charge rent amount
-    uint tenantCount;
-    uint sDepositFee;
-    uint rent_cost; //per month
-    
+
+    Tenant[] tenants; //list of tenants to auto charge rent amount
+    uint256 tenantCount;
+    uint256 sDepositFee;
+    uint256 rent_cost; //per month
 
     constructor(
-        uint _tenantCount,
-        uint _sDepositFee,
-        uint _rent_cost
+        uint256 _tenantCount,
+        uint256 _sDepositFee,
+        uint256 _rent_cost
     ) public {
         tenantCount = _tenantCount;
         sDepositFee = _sDepositFee;
@@ -26,18 +27,28 @@ contract aptContract {
     }
 
     //EVENTS
-    event rentPayment(address indexed _from, bytes32 indexed _id, uint _value);
+    event rentPayment(
+        address indexed _from,
+        bytes32 indexed _id,
+        uint256 _value
+    );
 
-    event securityDeposit(address indexed _from, bytes32 indexed _id, uint _value);
+    event securityDeposit(
+        address indexed _from,
+        bytes32 indexed _id,
+        uint256 _value
+    );
 
-    event depositRefund(address indexed _to, bytes32 indexed _id, uint _value);
+    event depositRefund(
+        address indexed _to,
+        bytes32 indexed _id,
+        uint256 _value
+    );
 
     //MODIFIERS
     //owner validation so only owner can set/change tenants list
-    modifier onlyOwner(){
-        require(
-            msg.sender == owner
-        ); 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
         _;
     }
 
@@ -47,22 +58,18 @@ contract aptContract {
             ); 
             _;
     }*/
-    
 
     //change rent_cost
-    function changeRent(uint new_rent_cost) public onlyOwner {
+    function changeRent(uint256 new_rent_cost) public onlyOwner {
         rent_cost = new_rent_cost;
     }
 
-    //charge all tenants rent amount  
-    function payRent() public onlyOwner{
-    }
+    //charge all tenants rent amount
+    function payRent() public onlyOwner {}
 
     //charge security deposit and add new tenant to tenant list
-    function newTenant() public onlyOwner {
-    }
+    function newTenant() public onlyOwner {}
 
     //return sDepositFee and remove tenant from list
-    function removeTenant() public onlyOwner {
-    }
+    function removeTenant() public onlyOwner {}
 }
